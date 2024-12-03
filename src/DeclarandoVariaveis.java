fa38c47a070d896158738521adc0a57ac4ef7617
gabriel1003 <gabriel.merconi@zup.com.br>
2024-12-02 21:02:17 -0300

    Adiciona as classes Main e DeclarandoVariaveis

---
+++ src/DeclarandoVariaveis.java
@@ -0,0 +1,11 @@
+public class DeclarandoVariaveis {
+    public static void main(String[] args) {
+
+        String name = "Gabriel Samersla Merçoni";
+        int idade = 27;
+        double altura = 1.67;
+        boolean matriculado = true;
+
+        System.out.printf("Nome: %s \n Idade: %d \n Altura: %.2f \n Matriculado: %b", name, idade, altura, matriculado);
+    }
+}
